40136fc4b3250747d2dbcfbb81131e8a1da6b519
Jimmy <nguyenzim098@gmail.com>
2025-09-16 21:30:08 +0900

    update Agents init

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,14 @@
 # from .utils import 
 from .need_analysis_agent import NeedsAnalysisAgent
 from .corpus_analysis_agent import CorpusAnalysisAgent
+from .level_analysis_agent import LevelAnalysisAgent
+from .syllabus_desginer_agent import SyllabusDesignerAgent
+from .module_generator_agent import ModuleGeneratorAgent
 
 __all__ = [
     "NeedsAnalysisAgent",
-    "CorpusAnalysisAgent"
+    "CorpusAnalysisAgent",
+    "LevelAnalysisAgent",
+    "SyllabusDesignerAgent",
+    "ModuleGeneratorAgent"
 ]
